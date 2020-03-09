01368c51c777c31cd9e4a3e325c860fe8e12934c
Hannah Melendy <hrmelendy@gmail.com>
2020-03-08 22:13:02 -0700

    add WIP REST API

--- httpChallenge.go
+++ httpChallenge.go
@@ -1,10 +1,5 @@
 /*- Write an HTTP server that functions as a key/value database.
 
-Users will make a POST request to /db to create or assign a value.
-User will make a GET request /db/<key> to get the value.
-Both request and response will be in JSON format.
-
-Use a map from string to the empty interface to hold values.
 And since we can't access the Go data structure from two different Go routines, limit the access to the map with a sync.Mutex.
 
 You can see an example of a POST body below. The key is x, and the value is 1.*/
@@ -16,6 +11,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // Data is the key and matching value from Json
@@ -25,7 +21,6 @@ type Data struct {
 }
 
 func main() {
-	data := &Data{}
 	var db = make(map[string]int)
 
 	dbPost := func(w http.ResponseWriter, r *http.Request) {
@@ -47,12 +42,30 @@ func main() {
 	dbGet := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		resp := &Data{"smelly", 43}
+		defer r.Body.Close()
+
+		path := r.URL.EscapedPath()
 
 		enc := json.NewEncoder(w)
-		if err := enc.Encode(resp); err != nil {
-			// Can't return error to client here
-			log.Printf("can't encode %v - %s", resp, err)
+
+		pathURL := strings.Split(path, "/db/")
+		keySlice := pathURL[0:]                 //obtain key
+		resultKey := strings.Join(keySlice, "") //convert array to string
+		fmt.Println(resultKey)
+		// look at map documentation and see if there's a method to check if key exists
+		if val, ok := db[resultKey]; ok {
+			fmt.Println("value: ", val)
+
+			fmt.Println(db[resultKey])
+
+			resp := &Data{resultKey, db[resultKey]}
+			if err := enc.Encode(resp); err != nil {
+				// Can't return error to client here
+				log.Printf("can't encode %v - %s", resp, err)
+			}
+			fmt.Println(resp)
+		} else {
+			fmt.Println("key not found")
 		}
 
 	}
@@ -69,14 +82,14 @@ func main() {
 		}
 	}
 
-	data.Key = "frangipani"
-	data.Value = 7
-
 	// db["lasagna"] = 78
 	// fmt.Println(db["lasagna"])
 
 	http.HandleFunc("/db", dbHandler)
-	http.HandleFunc("/db/{Key}", dbGet)
+	http.HandleFunc("/db/", dbGet)
+
+	// path := "/db/confusion"
+	// fmt.Println(path)
 
 	if err := http.ListenAndServe("127.0.0.1:8088", nil); err != nil {
 		log.Fatal(err)
